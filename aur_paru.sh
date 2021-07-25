3de649b4bd0aec429ad588342b12d8afa90392bd
Fabrizio <87930958+fabry76@users.noreply.github.com>
2021-07-25 14:52:32 +0200

    Add files via upload

---
+++ aur_paru.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#paru
+git clone https://aur.archlinux.org/paru.git /home/fabri/Downloads/paru
+cd /home/fabri/Downloads/paru
+makepkg -sri
+
+#aur
+paru -S ttf-ms-fonts timeshift visual-studio-code-bin --noconfirm
