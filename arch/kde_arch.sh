68e3370b8c4986337e1975ecafb0452d048ca7e6
fabry76 <fabrizio.fabiani@gmail.com>
2021-07-25 17:12:55 +0200

    headers

--- arch/kde_arch.sh
+++ arch/kde_arch.sh
@@ -11,7 +11,7 @@ locale-gen
 localectl set-locale LANGUAGE=en_US:en LC_TIME=it_IT.UTF-8 LC_MONETARY=it_IT.UTF-8 LC_MEASUREMENT=it_IT.UTF-8 LC_NUMERIC=it_IT.UTF-8 LC_PAPER=it_IT.UTF-8 LANG=en_US.UTF-8 LC_COLLATE=it_IT.UTF-8
 
 #install applications
-pacman -S --needed plasma plasma-wayland-session sddm dolphin nano firefox konsole sane cups nss-mdns vlc okular galculator shotwell htop curl vim cheese transmission-qt simple-scan libreoffice tlp net-tools linux-firmware chromium ark kate spectacle base-devel packagekit-qt5 pipewire pipewire-alsa ufw print-manager system-config-printer fuse gutenprint neofetch rust wget ksystemlog linux-headers virtualbox gnome-keyring --noconfirm
+pacman -S --needed plasma plasma-wayland-session sddm dolphin nano firefox konsole sane cups nss-mdns vlc okular galculator shotwell htop curl vim cheese transmission-qt simple-scan libreoffice tlp net-tools linux-firmware chromium ark kate spectacle base-devel packagekit-qt5 pipewire pipewire-alsa ufw print-manager system-config-printer fuse gutenprint neofetch rust wget ksystemlog virtualbox gnome-keyring linux-headers --noconfirm
 
 #pulseaudio workaround
 echo "options snd-hda-intel dmic_detect=0" | tee -a /etc/modprobe.d/alsa-base.conf
