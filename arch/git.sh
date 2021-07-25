59b7574fa0de6fc43752415f51adfad1c43a7d9d
fabry76 <fabrizio.fabiani@gmail.com>
2021-07-25 15:04:30 +0200

    prova

--- arch/git.sh
+++ arch/git.sh
@@ -2,4 +2,3 @@ git config --global user.email "fabrizio.fabiani@gmail.com"
 git config --global user.name "fabry76"
 git remote add linux https://github.com/fabry76/linux
 git push --set-upstream linux main
-prova
