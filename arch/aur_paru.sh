2bef3e9ebf905b1fb5df2ff2c1f27dbaf0423f2d
fabry76 <fabrizio.fabiani@gmail.com>
2021-07-26 16:23:41 +0200

    commit

--- arch/aur_paru.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#paru
-git clone https://aur.archlinux.org/paru.git /home/fabri/Downloads/paru
-cd /home/fabri/Downloads/paru
-makepkg -sri
-
-#aur
-paru -S ttf-ms-fonts timeshift visual-studio-code-bin --noconfirm
