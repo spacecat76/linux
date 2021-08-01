028f39bd98c8693dc5604d27ae5026438c69ad2d
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-01 19:06:24 +0200

    gnome

--- install/gnome_arch_pre.sh
+++ install/gnome_arch_pre.sh
@@ -11,10 +11,10 @@ locale-gen
 localectl set-locale LANGUAGE=en_US:en LC_TIME=it_IT.UTF-8 LC_MONETARY=it_IT.UTF-8 LC_MEASUREMENT=it_IT.UTF-8 LC_NUMERIC=it_IT.UTF-8 LC_PAPER=it_IT.UTF-8 LANG=en_US.UTF-8 LC_COLLATE=it_IT.UTF-8
 
 #install applications
-pacman -S --needed gnome-shell gdm gnome-control-center gnome-terminal gnome-software gnome-software-packagekit-plugin file-roller gedit nautilus gnome-tweaks nano firefox sane cups nss-mdns vlc htop curl vim cheese gnome-calculator gnome-photos totem gnome-screenshot gnome-logs gnome-system-monitor evince transmission-gtk simple-scan libreoffice tlp net-tools linux-firmware chromium base-devel ufw fuse gutenprint neofetch rust wget virtualbox gnome-keyring linux-headers --noconfirm
+pacman -S --needed gnome-shell gdm gnome-control-center gnome-terminal gnome-software gnome-software-packagekit-plugin file-roller gedit nautilus gnome-tweaks nano firefox sane cups nss-mdns htop curl vim cheese gnome-calculator gnome-photos gnome-music totem gnome-screenshot gnome-logs gnome-system-monitor evince transmission-gtk simple-scan libreoffice tlp net-tools linux-firmware chromium base-devel ufw fuse gutenprint neofetch rust wget virtualbox gnome-keyring linux-headers --noconfirm
 
 #pulseaudio workaround
-echo "options snd-hda-intel dmic_detect=0" | tee -a /etc/modprobe.d/alsa-base.conf
+#echo "options snd-hda-intel dmic_detect=0" | tee -a /etc/modprobe.d/alsa-base.conf
 
 #scanner
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
