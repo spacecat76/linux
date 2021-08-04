1649b725f092b419d96a1a4069be7a151f08d11b
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-04 10:21:53 +0200

    new

--- install/gnome_arch_pre.sh
+++ install/gnome_arch_pre.sh
@@ -1,15 +1,10 @@
 #install applications
 pacman -S --needed gnome-shell gdm gnome-control-center gnome-terminal gnome-software gnome-software-packagekit-plugin file-roller gedit nautilus gnome-tweaks nano firefox sane cups nss-mdns htop curl vim cheese gnome-calculator shotwell vlc gnome-screenshot gnome-logs tracker gnome-system-monitor evince transmission-gtk simple-scan libreoffice tlp net-tools chromium ufw fuse gutenprint neofetch rust wget virtualbox gnome-keyring linux-headers xdg-user-dirs gnome-weather ttf-ubuntu-font-family papirus-icon-theme --noconfirm
 
-#set aliases
-tee -a /home/fabri/.bashrc  << END
-alias ls="ls -lha --color"
-END
-
-#scanner
+#scanner resolution
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
 
-#printer
+#printer resolution
 sed -i 's/resolve/mdns_minimal [NOTFOUND=return] resolve/g' /etc/nsswitch.conf
 
 #ufw
@@ -20,9 +15,9 @@ tlp start
 
 #services
 systemctl disable bluetooth
-systemctl disable iptables.service
+systemctl disable iptables
 systemctl enable gdm
-systemctl enable avahi-daemon.service
+systemctl enable avahi-daemon
 systemctl enable cups
 systemctl enable ufw
 
