b97c3d23ed3bf16dd848df549aed2a6814f2d74a
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-06 12:08:24 +0200

    new

--- install/gnome_arch_pre.sh
+++ install/gnome_arch_pre.sh
@@ -1,9 +1,5 @@
 #install applications
-<<<<<<< HEAD
-pacman -S --needed gnome-shell gdm gnome-control-center gnome-terminal gnome-software gnome-software-packagekit-plugin file-roller gedit nautilus gnome-tweaks nano firefox sane cups nss-mdns htop curl vim cheese gnome-calculator shotwell vlc gnome-screenshot gnome-logs tracker gnome-system-monitor evince transmission-gtk simple-scan libreoffice tlp net-tools chromium ufw fuse gutenprint neofetch rust wget virtualbox gnome-keyring linux-lts-headers xdg-user-dirs gnome-weather ttf-ubuntu-font-family papirus-icon-theme --noconfirm
-=======
 pacman -S --needed gnome-shell gdm gnome-control-center gnome-terminal gnome-software gnome-software-packagekit-plugin file-roller gedit nautilus gnome-tweaks firefox sane cups nss-mdns htop curl vim cheese gnome-calculator shotwell vlc gnome-screenshot gnome-logs tracker gnome-system-monitor evince transmission-gtk simple-scan libreoffice tlp net-tools chromium ufw fuse gutenprint neofetch rust wget virtualbox gnome-keyring linux-lts-headers xdg-user-dirs gnome-weather ttf-ubuntu-font-family papirus-icon-theme --noconfirm
->>>>>>> 09b2ffbf648766fa49b0a6c84066f7370e6044ee
 
 #scanner resolution
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
