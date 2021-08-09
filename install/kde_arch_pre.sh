b581cf291b50f53deb3eb091d8e20a2648a75332
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-09 15:39:11 +0200

    new

--- install/kde_arch_pre.sh
+++ install/kde_arch_pre.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #install applications
-pacman -S --needed plasma plasma-wayland-session sddm dolphin firefox konsole sane cups nss-mdns vlc okular galculator shotwell htop curl vim cheese transmission-qt simple-scan libreoffice tlp net-tools chromium ark kate spectacle packagekit-qt5 ufw print-manager system-config-printer fuse gutenprint neofetch rust wget ksystemlog gnome-keyring linux-lts-headers ttf-ubuntu-font-family papirus-icon-theme --noconfirm
+pacman -S --needed plasma plasma-wayland-session sddm dolphin firefox konsole sane cups nss-mdns vlc okular galculator shotwell htop curl vim cheese transmission-qt simple-scan libreoffice tlp net-tools chromium ark kate spectacle packagekit-qt5 ufw print-manager system-config-printer fuse gutenprint neofetch rust wget ksystemlog gnome-keyring linux-lts-headers ttf-ubuntu-font-family papirus-icon-theme bash-completion --noconfirm
 
 #scanner
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
