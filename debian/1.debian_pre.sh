2eec8b583e02336f1933546278ae7a79fa36bebf
Fabrizio <fabri@arco.localdomain>
2021-08-18 18:54:54 +0200

    new

--- debian/1.debian_pre.sh
+++ debian/1.debian_pre.sh
@@ -5,11 +5,11 @@ sed -i 's/main/main contrib non-free/g' /etc/apt/sources.list
 apt update
 
 #install common app
-apt install flatpak libreoffice vlc sane cups htop curl vim simple-scan tlp net-tools chromium ufw neofetch printer-driver-all papirus-icon-theme timeshift ttf-mscorefonts-installer shotwell -y
+apt install flatpak libreoffice vlc sane cups htop curl vim simple-scan tlp net-tools chromium ufw neofetch printer-driver-all papirus-icon-theme timeshift ttf-mscorefonts-installer shotwell firmware-sof-signed apt-transport-https -y
 
 #flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install firefox
+flatpak install flathub org.mozilla.firefox -y
 
 #scanner
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
