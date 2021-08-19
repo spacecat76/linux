2ef7460bb32eff0eef5913c7355521272c964127
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-19 15:19:05 +0200

    new

--- debian/1.debian_pre.sh
+++ debian/1.debian_pre.sh
@@ -1,5 +1,8 @@
 #install common app
-apt install libreoffice vlc sane cups htop curl vim simple-scan tlp net-tools pavucontrol ufw neofetch printer-driver-all papirus-icon-theme timeshift ttf-mscorefonts-installer shotwell firmware-sof-signed apt-transport-https -y
+apt install libreoffice vlc sane cups printer-driver-cups-pdf htop curl vim simple-scan tlp net-tools pavucontrol ufw neofetch printer-driver-all papirus-icon-theme timeshift ttf-mscorefonts-installer shotwell firmware-sof-signed apt-transport-https plasma-nm -y
+
+#add user to groups
+sudo usermod -a -G lpadmin fabri
 
 #scanner
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
