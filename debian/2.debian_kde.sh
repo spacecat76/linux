5094984e42a3227e6e4fbb31d0755d7d2aa9d5bc
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-19 21:17:04 +0200

    new

--- debian/2.debian_kde.sh
+++ debian/2.debian_kde.sh
@@ -12,6 +12,18 @@ tee -a /home/fabri/.config/kwalletrc  << END
 Enabled=false
 END
 
+#touchpad X11
+tee -a /etc/X11/xorg.conf.d/30-touchpad.conf  << END
+Section "InputClass"
+Identifier "touchpad"
+Driver "libinput"
+  MatchIsTouchpad "on"
+  Option "Tapping" "on"
+  Option "NaturalScrolling" "on"
+  Option "ClickMethod" "clickfinger"
+EndSection
+END
+
 #setting permission to home folder
 chown -R fabri:fabri /home/fabri/
 
