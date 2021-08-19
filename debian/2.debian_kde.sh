bf5ef587c4e999e8b4c783282018f899dff23633
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-19 16:48:17 +0200

    new

--- debian/2.debian_kde.sh
+++ debian/2.debian_kde.sh
@@ -1,11 +1,16 @@
 #install KDE
-apt install kde-plasma-desktop libreoffice-kde5 kamoso okular galculator transmission-qt ark kate kde-spectacle print-manager ksystemlog kolourpaint gnome-keyring -y
+apt install kde-plasma-desktop libreoffice-kde5 kamoso okular galculator transmission-qt ark kate kde-spectacle print-manager ksystemlog kolourpaint gnome-keyring  plasma-nm -y
 
 #remove uneeded KDE applications
 apt remove konqueror termit kdeconnect kwrite kwalletmanager -y
 
 #cleanup
 apt autoremove -y
+
+#kdewallet
+tee -a /home/fabri/.config/kwalletrc  << END
+Enabled=false
+END
 
 #setting permission to home folder
 chown -R fabri:fabri /home/fabri/
