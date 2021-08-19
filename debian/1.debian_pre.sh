db9a5ee53ab3c271f1aa67dea3f76bd8185265e7
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-19 14:03:43 +0200

    new

--- debian/1.debian_pre.sh
+++ debian/1.debian_pre.sh
@@ -1,15 +1,5 @@
-#enable non-free
-sed -i 's/main/main contrib non-free/g' /etc/apt/sources.list
-
-#update repos
-apt update
-
 #install common app
-apt install flatpak libreoffice vlc sane cups htop curl vim simple-scan tlp net-tools chromium ufw neofetch printer-driver-all papirus-icon-theme timeshift ttf-mscorefonts-installer shotwell firmware-sof-signed apt-transport-https -y
-
-#flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.mozilla.firefox -y
+apt install libreoffice vlc sane cups htop curl vim simple-scan tlp net-tools pavucontrol ufw neofetch printer-driver-all papirus-icon-theme timeshift ttf-mscorefonts-installer shotwell firmware-sof-signed apt-transport-https -y
 
 #scanner
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
---
+++ pop_os.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#install applications
+apt install ttf-mscorefonts-installer code timeshift neofetch tlp ufw curl wget htop transmission cheese google-chrome-stable -y
+
+#scanner
+echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
+
+#ufw
+ufw enable
+
+#tlp
+tlp start
+
+#services
+systemctl disable bluetooth
+systemctl enable ufw
