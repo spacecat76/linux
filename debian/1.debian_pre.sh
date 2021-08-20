28cd02fc4113cf75c5a38669bb51feac887a3360
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-20 18:32:24 +0200

    new

--- debian/1.debian_pre.sh
+++ debian/1.debian_pre.sh
@@ -1,5 +1,5 @@
 #install common app
-apt install libreoffice-calc libreoffice-impress libreoffice-writer sane cups printer-driver-all printer-driver-cups-pdf htop curl vim simple-scan tlp net-tools ufw neofetch papirus-icon-theme timeshift ttf-mscorefonts-installer firmware-sof-signed apt-transport-https -y
+apt install libreoffice-calc libreoffice-impress libreoffice-writer sane cups printer-driver-all printer-driver-cups-pdf htop curl vim simple-scan tlp net-tools ufw neofetch papirus-icon-theme timeshift ttf-mscorefonts-installer firmware-sof-signed apt-transport-https firmware-realtek -y
 
 #add user to group
 sudo usermod -a -G lpadmin fabri
