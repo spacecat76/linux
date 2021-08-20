4bc6625fa911cc41229d008b8de850a75c27bd72
Fabrizio <87930958+fabry76@users.noreply.github.com>
2021-08-20 09:10:36 +0200

    Update 2.debian_kde.sh

--- debian/2.debian_kde.sh
+++ debian/2.debian_kde.sh
@@ -1,5 +1,5 @@
 #install KDE
-apt install kde-plasma-desktop libreoffice-kde5 kamoso okular galculator transmission-qt ark kate kde-spectacle print-manager ksystemlog kolourpaint gnome-keyring  plasma-nm -y
+apt install kde-plasma-desktop libreoffice-kde5 kamoso okular galculator transmission-qt ark kate kde-spectacle print-manager ksystemlog kolourpaint gnome-keyring plasma-nm -y
 
 #remove uneeded KDE applications
 apt remove konqueror termit kdeconnect kwrite kwalletmanager -y
