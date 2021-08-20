aa5ceb2a12ff2eb7265bce0726f938334293f019
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-20 14:42:33 +0200

    new

--- debian/1.debian_pre.sh
+++ debian/1.debian_pre.sh
@@ -1,5 +1,5 @@
 #install common app
-apt install firefox-esr chromium libreoffice vlc sane cups printer-driver-all printer-driver-cups-pdf htop curl vim simple-scan tlp net-tools pavucontrol ufw neofetch papirus-icon-theme timeshift ttf-mscorefonts-installer shotwell firmware-sof-signed apt-transport-https -y
+apt install libreoffice-calc libreoffice-impress libreoffice-writer sane cups printer-driver-all printer-driver-cups-pdf htop curl vim simple-scan tlp net-tools ufw neofetch papirus-icon-theme timeshift ttf-mscorefonts-installer firmware-sof-signed apt-transport-https -y
 
 #add user to group
 sudo usermod -a -G lpadmin fabri
--- debian/2.debian_gnome.sh
+++ debian/2.debian_gnome.sh
@@ -1,4 +1,19 @@
+#install gnome
+apt install gnome-core avahi-daemon network-manager-gnome libreoffice-gnome cheese transmission-gtk file-roller gnome-screenshot gnome-tweaks gnome-weather gnome-calendar gnome-clocks gnome-photos -y
+
+#delete gnome extensions
+rm -rf /usr/share/gnome-shell/extensions/*
+
+#install dash-to-panel
+apt install gnome-shell-extension-dash-to-panel -y
+
 #chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /home/fabri/Downloads
-apt install /home/fabri/Downloads/google-chrome-stable_current_amd64.deb
+apt install /home/fabri/Downloads/google-chrome-stable_current_amd64.deb -y
 rm /home/fabri/Downloads/*.deb
+
+#cleanup
+apt autoremove -y
+
+#setting permission to home folder
+chown -R fabri:fabri /home/fabri/
