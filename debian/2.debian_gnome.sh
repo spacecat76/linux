33d89327bdea93a9958f6367e783886ec773eee0
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-22 11:08:21 +0200

    new

--- debian/2.debian_gnome.sh
+++ debian/2.debian_gnome.sh
@@ -1,6 +1,9 @@
 #install gnome
 apt install gnome-core avahi-daemon network-manager-gnome libreoffice-gnome cheese transmission-gtk file-roller gnome-screenshot gnome-tweaks gnome-weather gnome-calendar gnome-clocks gnome-photos -y
 
+#remove uneeded gnome applications
+sudo apt remove malcontent gnome-contacts termit
+
 #delete gnome extensions
 rm -rf /usr/share/gnome-shell/extensions/*
 
