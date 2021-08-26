c13173656c8e28b1227848f7345d7606099d5c1f
fabry76 <fabrizio.fabiani@gmail.com>
2021-08-26 23:33:21 +0200

    new

--- debian/1.debian_pre.sh
+++ debian/1.debian_pre.sh
@@ -1,5 +1,5 @@
 #install common app
-apt install libreoffice-calc libreoffice-impress libreoffice-writer sane cups printer-driver-all printer-driver-cups-pdf htop curl vim simple-scan tlp net-tools ufw neofetch papirus-icon-theme timeshift ttf-mscorefonts-installer firmware-sof-signed apt-transport-https firmware-realtek intel-microcode stacer make -y
+apt install sane cups printer-driver-all printer-driver-cups-pdf htop curl vim simple-scan tlp net-tools firewalld firewall-config neofetch papirus-icon-theme timeshift ttf-mscorefonts-installer firmware-sof-signed apt-transport-https firmware-realtek intel-microcode stacer make -y
 
 #add user to group
 sudo usermod -a -G lpadmin fabri
@@ -7,13 +7,10 @@ sudo usermod -a -G lpadmin fabri
 #scanner
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
 
-#ufw
-ufw enable
-
 #tlp
 tlp start
 
 #services
 systemctl disable bluetooth
 systemctl enable cups
-systemctl enable ufw
+systemctl enable firewalld
