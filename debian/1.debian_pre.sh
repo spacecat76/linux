3bb5f938c3bd24d3cae71d4810a49663ae2e2da0
fabry76 <fabrizio.fabiani@gmail.com>
2021-09-08 13:00:15 +0200

    new

--- debian/1.debian_pre.sh
+++ debian/1.debian_pre.sh
@@ -27,23 +27,6 @@ systemctl enable avahi-daemon
 #firewall
 firewall-cmd --set-default-zone=home
 
-#virtualbox
-tee -a /etc/apt/sources.list  << END
-deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian bullseye contrib
-END
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
-apt update
-apt install virtualbox-6.1 -y
-
-#code
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-apt update
-apt install code -y
-
 #chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /home/fabri/Downloads
 apt install /home/fabri/Downloads/google-chrome-stable_current_amd64.deb -y
