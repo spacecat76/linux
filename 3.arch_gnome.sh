6f6f993fd613159af9e703c986de141902f24d1c
fabry76 <fabrizio.fabiani@gmail.com>
2021-09-10 12:22:44 +0200

    new

--- 3.arch_gnome.sh
+++
@@ -1,5 +0,0 @@
-#install gnome applications
-pacman -S --needed gnome-shell gdm gnome-control-center gnome-terminal gnome-software gnome-software-packagekit-plugin file-roller gedit nautilus gnome-tweaks gnome-calculator gnome-screenshot gnome-logs tracker gnome-system-monitor evince transmission-gtk gnome-weather gnome-photos --noconfirm
-
-#enable gdm
-systemctl enable gdm
