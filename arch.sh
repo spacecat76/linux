526cba01849b6fb5740c24e8acf56da5c87aac1b
fabry76 <fabrizio.fabiani@gmail.com>
2021-11-11 20:10:07 +0100

    new

--- arch.sh
+++ arch.sh
@@ -1,32 +1,5 @@
-#!/bin/bash
-
-#jumpto
-function jumpto
-{
-    label=$1
-    cmd=$(sed -n "/$label:/{:a;n;p;ba};" $0 | grep -v ':$')
-    eval "$cmd"
-    exit
-}
-start=${1:-"start"}
-
-jumpto $start
-
-#de selection
-start:
-echo "Which DE would you like to install? (gnome, kde, none or exit)"
-read de
-if [[ $de == "gnome" ]] || [[ $de == "kde" ]] || [[ $de == "none" ]]; then
-   jumpto install
-elif [[ $de == "exit" ]]; then
-   exit
-else
-   jumpto start
-fi
-install:
-
 #install applications
-pacman -S --needed firefox sane cups nss-mdns htop curl vim cheese vlc simple-scan libreoffice tlp net-tools chromium firewalld fuse gutenprint neofetch rust wget virtualbox linux-lts-headers ttf-ubuntu-font-family papirus-icon-theme bash-completion sof-firmware appstream gimp --noconfirm
+pacman -S --needed firefox sane cups nss-mdns htop curl vim cheese vlc simple-scan libreoffice tlp net-tools firewalld fuse gutenprint neofetch rust wget linux-lts-headers ttf-ubuntu-font-family papirus-icon-theme bash-completion sof-firmware appstream gimp --noconfirm
 
 #scanner
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
@@ -46,32 +19,12 @@ systemctl enable cups
 #firewall
 firewall-cmd --set-default-zone=home
 
-#de install
-if [[ $de == "gnome" ]]; then
-   jumpto gnome
-elif [[ $de == "kde" ]]; then
-   jumpto kde
-else
-   jumpto final
-fi
-
-gnome:
-#install gnome applications
-pacman -S --needed gnome-shell gdm gnome-control-center gnome-terminal gnome-software gnome-software-packagekit-plugin file-roller gedit nautilus gnome-tweaks gnome-calculator gnome-screenshot gnome-logs tracker gnome-system-monitor evince transmission-gtk gnome-weather gnome-photos gnome-builder --noconfirm
-
-#enable gdm
-systemctl enable gdm
-
-jumpto final
-
-kde:
 #install kde applications
-pacman -S --needed plasma plasma-wayland-session sddm dolphin konsole okular galculator transmission-qt ark kate spectacle packagekit-qt5 print-manager system-config-printer ksystemlog kdevelop shotwell --noconfirm
+pacman -S --needed plasma plasma-wayland-session sddm thunar alacritty okular galculator transmission-qt ark kate spectacle packagekit-qt5 print-manager system-config-printer ksystemlog kdevelop shotwell --noconfirm
 
 #enable sddm
 systemctl enable sddm
 
-final:
 #set x11 KB language (login manager)
 localectl set-x11-keymap it
 
