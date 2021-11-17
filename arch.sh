cccfca757810c80dbb04009c210fb07d724f1cca
fabry76 <fabrizio.fabiani@gmail.com>
2021-11-17 21:17:59 +0100

    new

--- arch.sh
+++ arch.sh
@@ -30,16 +30,29 @@ firewall-cmd --set-default-zone=home
 pacman -S --needed plasma sddm dolphin konsole okular galculator transmission-qt ark kate spectacle packagekit-qt5 print-manager system-config-printer ksystemlog kdevelop partitionmanager kamoso --noconfirm
 
 #remove kde application
-pacman -R plasma-browser-integration plasma-firewall -y
+pacman -R plasma-browser-integration plasma-firewall --noconfirm
 
 #install flatpak
 flatpak install flathub org.gtk.Gtk3theme.Breeze-Dark io.gitlab.librewolf-community -y
+flatpak override --env=GTK_THEME=Breeze-Dark
 
 #enable sddm
 systemctl enable sddm
 
 #set x11 KB language (login manager)
 localectl set-x11-keymap it
+
+#touchpad X11
+tee -a /etc/X11/xorg.conf.d/30-touchpad.conf  << END
+Section "InputClass"
+Identifier "touchpad"
+Driver "libinput"
+  MatchIsTouchpad "on"
+  Option "Tapping" "on"
+  Option "NaturalScrolling" "on"
+  Option "ClickMethod" "clickfinger"
+EndSection
+END
 
 #setting permission to home folder
 chown -R fabri:fabri /home/fabri/
