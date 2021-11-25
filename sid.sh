5736657725ce652024c36fcb02cbd5f229ef1809
fabry76 <fabrizio.fabiani@gmail.com>
2021-11-25 16:45:22 +0100

    new

---
+++ sid.sh
@@ -0,0 +1,64 @@
+apt update
+
+#install common apps
+apt install firefox sane cups avahi-daemon printer-driver-all printer-driver-cups-pdf htop curl vim tlp net-tools firewalld firewall-config neofetch timeshift ttf-mscorefonts-installer firmware-sof-signed apt-transport-https firmware-realtek intel-microcode build-essential flatpak python3-pip fonts-crosextra-carlito fonts-crosextra-caladea mlocate unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-vaapi simple-scan -y
+
+#add user to group
+usermod -a -G lpadmin fabri
+
+#scanner
+echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
+
+#tlp
+tlp start
+
+#grub
+sed -i 's/quiet//g' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+update-grub
+
+#services
+systemctl disable bluetooth
+systemctl enable cups
+systemctl enable firewalld
+systemctl enable avahi-daemon
+
+#firewall
+firewall-cmd --set-default-zone=home
+
+
+#install gnome
+apt install gnome-core file-roller gnome-software-plugin-flatpak gnome-screenshot gnome-tweaks gnome-weather gnome-calendar gnome-clocks gnome-photos -y
+
+#remove uneeded gnome applications
+apt remove --purge malcontent termit gnome-contacts xterm firefox-esr -y
+
+#cleanup extensions
+rm -rf /usr/share/gnome-shell/extensions/*
+
+#dashtopanel
+apt install gnome-shell-extension-dash-to-panel -y
+
+#network manager
+sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
+
+#touchpad X11
+tee -a /etc/X11/xorg.conf.d/30-touchpad.conf  << END
+Section "InputClass"
+Identifier "touchpad"
+Driver "libinput"
+  MatchIsTouchpad "on"
+  Option "Tapping" "on"
+  Option "NaturalScrolling" "on"
+  Option "ClickMethod" "clickfinger"
+EndSection
+END
+
+#set x11 KB language (GDM)
+localectl set-x11-keymap it
+
+#cleanup packages
+apt autoremove -y
+
+#setting permission to home folder
+chown -R fabri:fabri /home/fabri/
