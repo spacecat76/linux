835692220fc076bea2e5d15e273959856a9b988c
fabry76 <fabrizio.fabiani@gmail.com>
2021-12-04 12:45:31 +0100

    new

--- popos.sh
+++ popos.sh
@@ -1,5 +1,5 @@
 #install applications
-apt install ttf-mscorefonts-installer timeshift neofetch htop gnome-photos printer-driver-cups-pdf firewalld firewall-config apt-transport-https python3-pip fonts-crosextra-carlito fonts-crosextra-caladea mlocate code transmission cheese virt-manager google-chrome-stable -y
+apt install ttf-mscorefonts-installer timeshift neofetch htop gnome-photos printer-driver-cups-pdf firewalld firewall-config apt-transport-https python3-pip fonts-crosextra-carlito fonts-crosextra-caladea mlocate code transmission cheese virt-manager gimp google-chrome-stable -y
 
 #scanner
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
