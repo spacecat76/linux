f546bb5e96358b8095c4bc546b57e6534882d001
fabry76 <fabrizio.fabiani@gmail.com>
2021-12-04 12:44:29 +0100

    new

--- popos.sh
+++ popos.sh
@@ -1,20 +1,11 @@
-#!/bin/bash
-
-#remove applications
-apt remove geary -y
-
 #install applications
-apt install ttf-mscorefonts-installer timeshift neofetch tlp curl wget htop gnome-photos printer-driver-cups-pdf net-tools firewalld firewall-config apt-transport-https stacer make python3-pip fonts-crosextra-carlito fonts-crosextra-caladea mlocate code transmission cheese google-chrome-stable -y
+apt install ttf-mscorefonts-installer timeshift neofetch htop gnome-photos printer-driver-cups-pdf firewalld firewall-config apt-transport-https python3-pip fonts-crosextra-carlito fonts-crosextra-caladea mlocate code transmission cheese virt-manager google-chrome-stable -y
 
 #scanner
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
 
-#tlp
-tlp start
-
 #services
 systemctl disable bluetooth
-systemctl enable cups
 systemctl enable firewalld
 
 #firewall
@@ -23,5 +14,3 @@ firewall-cmd --set-default-zone=home
 #cleanup
 apt autoremove -y
 
-#setting permission to home folder
-chown -R fabri:fabri /home/fabri/
