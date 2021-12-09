7094672a0d8bba9848589189519c7483ce90f3e2
fabry76 <fabrizio.fabiani@gmail.com>
2021-12-09 23:20:17 +0100

    new

--- arch_xfce.sh
+++ arch_xfce.sh
@@ -8,7 +8,7 @@ pacman -S --needed htop curl tlp neofetch rust wget linux-lts-headers bash-compl
 pacman -S --needed ttf-ubuntu-font-family ttf-opensans ttf-carlito ttf-caladea ttf-liberation ttf-inconsolata ttf-fira-code ttf-fira-mono ttf-fira-sans --noconfirm
 
 #applications
-pacman -S --needed firefox vim vlc libreoffice-fresh gimp simple-scan --noconfirm
+pacman -S --needed firefox vim vlc libreoffice-fresh gimp simple-scan virtualbox --noconfirm
 
 #network
 pacman -S --needed network-manager-applet nss-mdns inetutils net-tools avahi-daemon --noconfirm
@@ -24,11 +24,6 @@ pacman -S --needed sane cups gutenprint --noconfirm
 systemctl enable cups
 sed -i 's/resolve/mdns_minimal [NOTFOUND=return] resolve/g' /etc/nsswitch.conf
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
-
-#virt manager
-pacman -S --needed virt-manager libirt --noconfirm
-adduser fabri libvirt
-virsh net-autostart default
 
 #grub
 sed -i 's/loglevel=3 //g' /etc/default/grub
