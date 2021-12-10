3441b87e829fc533f1319c467ecec15e86a92ded
fabry76 <fabrizio.fabiani@gmail.com>
2021-12-10 17:45:53 +0100

    new

--- arch_xfce.sh
+++ arch_xfce.sh
@@ -1,14 +1,14 @@
 #desktop environment
-pacman -S xfce4 xfce4-dev-tools lighdm lightdm-gtk-greeter lightdm-slick-greeter xfce4-screenshooter xfce4-terminal xarchiver thunar-archive-plugin transmission-gtk xfce4-battery-plugin xfce4-datetime-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin gthumb galculator mousepad gparted redshift gvfs --noconfirm
+pacman -S xfce4 xfce4-dev-tools xorg-server-xephyr xfce4-screenshooter xfce4-terminal xarchiver thunar-archive-plugin transmission-gtk xfce4-battery-plugin xfce4-datetime-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin gthumb galculator mousepad gparted redshift gvfs --noconfirm
 
 #utilities
-pacman -S --needed htop curl tlp neofetch rust wget linux-lts-headers bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse ffmpeg --noconfirm
+pacman -S --needed htop curl tlp neofetch rust wget linux-lts-headers bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse ffmpeg nano --noconfirm
 
 #fonts
 pacman -S --needed ttf-ubuntu-font-family ttf-opensans ttf-carlito ttf-caladea ttf-liberation ttf-inconsolata ttf-fira-code ttf-fira-mono ttf-fira-sans --noconfirm
 
 #applications
-pacman -S --needed firefox vim vlc libreoffice-fresh gimp simple-scan virtualbox --noconfirm
+pacman -S --needed firefox vim vlc libreoffice-fresh gimp simple-scan --noconfirm
 
 #network
 pacman -S --needed network-manager-applet nss-mdns inetutils net-tools avahi-daemon --noconfirm
@@ -25,6 +25,10 @@ systemctl enable cups
 sed -i 's/resolve/mdns_minimal [NOTFOUND=return] resolve/g' /etc/nsswitch.conf
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
 
+#virt-manager
+pacman -S --needed virt-manager virtlib --noconfirm
+systemctl enable virtlibd
+
 #grub
 sed -i 's/loglevel=3 //g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
@@ -33,7 +37,11 @@ grub-mkconfig -o /boot/grub/grub.cfg
 #set x11 KB language (login manager)
 localectl set-x11-keymap it
 
+#lightdm
+pacman -S --needed lightdm lightdm-gtk-greeter lightdm-slick-greeter --noconfirm
+systemctl enable lightdm
+sed -i 's/# greeter-session/greeter-session=lightdm-slick-greeter/g' /etc/lightdm/lightdm.conf
+
 #various
 systemctl disable bluetooth
-systemctl enable lightdm
 tlp start
