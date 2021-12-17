9049230f81e581209719e6295b47494861279cd5
fabry76 <fabrizio.fabiani@gmail.com>
2021-12-18 00:16:35 +0100

    new

--- arch_aur.sh
+++ arch_aur.sh
@@ -4,6 +4,6 @@ cd /home/fabri/Downloads/paru
 makepkg -sri
 
 #install aur applications
-paru -S ttf-ms-fonts librewolf-bin pamac-aur visual-studio-code-bin lightdm-settings timeshift-bin --noconfirm
+paru -S ttf-ms-fonts librewolf-bin pamac-aur visual-studio-code-bin timeshift-bin --noconfirm
 
-#paru -S webcamoid mugshot xfce4-docklike-plugin --noconfirm
+#paru -S webcamoid mugshot xfce4-docklike-plugin lightdm-settings --noconfirm
