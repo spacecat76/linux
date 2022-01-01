194a3754b5359f02cc2dbc45ae8504f46056bba4
fabry76 <fabrizio.fabiani@gmail.com>
2022-01-01 19:00:48 +0100

    new

--- arch.sh
+++ arch.sh
@@ -1,9 +1,6 @@
 #kde
 pacman -S --needed plasma plasma-wayland-session plasma-wayland-protocols sddm xarchiver thunar thunar-archive-plugin konsole okular galculator transmission-qt kate spectacle packagekit-qt5 print-manager system-config-printer ksystemlog partitionmanager kamoso gwenview kwallet-pam kwalletmanager --noconfirm
 
-#kde unneeded apps
-pacman -R plasma-browser-integration plasma-firewall --noconfirm
-
 #utilities
 pacman -S --needed htop curl tlp neofetch rust wget linux-headers bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse ffmpeg --noconfirm
 
@@ -29,15 +26,14 @@ systemctl enable cups
 sed -i 's/resolve/mdns_minimal [NOTFOUND=return] resolve/g' /etc/nsswitch.conf
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
 
+#remove components
+pacman -R bluez bluedevil plasma-browser-integration plasma-firewall --noconfirm 
+
 #grub
 sed -i 's/loglevel=3 //g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-#set x11 KB language (login manager)
-localectl set-x11-keymap it
-
 #various
-systemctl disable bluetooth
 systemctl enable sddm
 tlp start
