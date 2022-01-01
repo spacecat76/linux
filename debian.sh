ff3fdfa5d7ab85b35cb0ed5b0e350c1c41c10c1a
fabry76 <fabrizio.fabiani@gmail.com>
2022-01-01 21:20:23 +0100

    new

--- debian.sh
+++ debian.sh
@@ -48,14 +48,6 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -
 apt install /home/fabri/google-chrome-stable_current_amd64.deb -y
 rm -f /home/fabri/google-chrome-stable_current_amd64.deb
 
-#vs code
-#wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-#install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-#sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-#rm -f packages.microsoft.gpg
-#apt update
-#apt install code -y
-
 #purge components
 apt purge bluez avahi-autoipd totem malcontent gnome-contacts virt-viewer -y
 
