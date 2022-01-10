782091ed45677df1ab3612ad18649232a6b2f329
fabry76 <fabrizio.fabiani@gmail.com>
2022-01-10 22:03:56 +0100

    new

--- arch.sh
+++ arch.sh
@@ -37,6 +37,10 @@ systemctl enable cups
 sed -i 's/resolve/mdns_minimal [NOTFOUND=return] resolve/g' /etc/nsswitch.conf
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
 
+#disable pipewire
+systemctl disable --global pipewire
+rm -rf /home/fabri/.config/pulse
+
 #grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
