b5abd97f53686c44b7dfccef9789d9aeccb1f0b3
fabry76 <fabrizio.fabiani@gmail.com>
2022-01-12 20:11:46 +0100

    new

--- debian.sh
+++ debian.sh
@@ -25,8 +25,10 @@ apt install vim htop neofetch timeshift transmission-gtk vlc shotwell gimp -y
 #network
 apt install avahi-daemon ufw -y
 systemctl enable avahi-daemon
-systemctl enable ufw
 sed -i 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
+sed -i 's/DefaultDependencies=no/Wants=network-pre.target/g' /lib/systemd/system/ufw.service
+sed -i 's/Before=network.target/Before=network-pre.target/g' /lib/systemd/system/ufw.service
+systemctl enable ufw
 ufw allow mdns
 sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
 
