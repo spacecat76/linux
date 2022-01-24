08617310d66568ad2e71eb0f2250e586c6ac0de9
fabry76 <fabrizio.fabiani@gmail.com>
2022-01-24 19:41:02 +0100

    new

--- debian.sh
+++ debian.sh
@@ -47,8 +47,7 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-apt update
-apt install code -y
+apt update && apt install code -y
 
 # flatpak
 apt install flatpak gnome-software-plugin-flatpak -y
