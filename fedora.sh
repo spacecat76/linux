1bfd8d0a11c127911a9c2fb54713cd76a04a6f92
fabry76 <fabrizio.fabiani@gmail.com>
2022-04-09 21:00:48 +0200

    new

---
+++ fedora.sh
@@ -0,0 +1,42 @@
+# dnf
+tee -a /etc/dnf/dnf.conf << END
+max_parallel_downloads=10
+fastestmirror=True
+END
+
+# update
+dnf update -y
+
+# rpm fusion
+dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+
+dnf install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+# media codecs
+dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
+dnf install lame\* --exclude=lame-devel -y
+dnf group upgrade --with-optional Multimedia -y
+
+# install package
+dnf install google-chrome-stable gimp vlc timeshift neofetch htop shotwell python3-pip vim gnome-tweaks transmission geary -y
+
+# flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.gtk.Gtk3theme.Adwaita-dark com.system76.Popsicle com.mattjakeman.ExtensionManager -y
+
+# vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf update -y
+dnf install code -y
+
+#firewall
+
+
+# virt manager
+dnf install virt-manager -y
+
+# printing and scanning
+echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
