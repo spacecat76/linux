ec17fce5c0d11b4f6ad556bc3f90b468ce8d4a92
fabry76 <fabrizio.fabiani@gmail.com>
2022-04-24 08:21:06 +0200

    new

--- debian_testing.sh
+++ debian_testing.sh
@@ -14,17 +14,16 @@ apt install gimp vim htop neofetch timeshift vlc papirus-icon-theme net-tools cu
 apt install ttf-mscorefonts-installer fonts-ubuntu fonts-crosextra-carlito fonts-crosextra-caladea fonts-firacode -y
 
 # network
-apt install avahi-daemon gufw -y
-systemctl enable avahi-daemon
-systemctl enable ufw --now
+apt install init-system-helpers adduser bind9-host dbus  libavahi-common3 libavahi-core7 libc6 libcap2 libdaemon0 libdbus-1-3 libexpat1 lsb-base libnss-mdns gufw -y
+#systemctl enable avahi-daemon
+#systemctl enable ufw --now
 ufw enable
 ufw allow mdns
-sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
+#sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
 
 # virt manager
 apt install virt-manager -y
 adduser fabri libvirt
-virsh net-autostart default
 
 # printing and scanning
 apt install sane cups printer-driver-all printer-driver-cups-pdf simple-scan -y
