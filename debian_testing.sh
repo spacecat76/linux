925a7dbd6eef9dbb2a0c8aab710cccc20494c1e2
fabry76 <fabrizio.fabiani@gmail.com>
2022-05-05 08:43:20 +0200

    new

--- debian_testing.sh
+++ debian_testing.sh
@@ -69,10 +69,11 @@ sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 update-grub
 
 # fastgate
-#apt install cifs-utils smbclient -y
-#tee -a /etc/fstab  << END
-#//192.168.1.254/samba/usb1_1 /home/fabri/Fastgate cifs user=admin,vers=1.0,dir_mode=0777,file_mode=0777,pass=admin
-#END
+apt install cifs-utils smbclient -y
+tee -a /etc/fstab  << END
+# map fastgate usb storage
+//192.168.1.254/samba/usb1_1 /home/fabri/Fastgate cifs user=admin,vers=1.0,dir_mode=0777,file_mode=0777,pass=admin
+END
 
 ## optional
 # purge components
