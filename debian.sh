66039e79617b736243ee7516b524af7ca9d4aa9b
fabry76 <fabrizio.fabiani@gmail.com>
2022-06-25 09:22:40 +0200

    new

--- debian.sh
+++ debian.sh
@@ -1,13 +1,13 @@
-# apt sources
-mv /etc/apt/sources.list /etc/apt/sources.list.old
-cp /home/fabri/Git/linux/conf/sources.list /etc/apt/sources.list
+# apt sources TESTING
+#mv /etc/apt/sources.list /etc/apt/sources.list.old
+#cp /home/fabri/Git/linux/conf/sources.list /etc/apt/sources.list
 
-# apt preferences
-tee -a /etc/apt/preferences  << END
-Package: rsync
-Pin: release n=bullseye
-Pin-Priority: 900
-END
+# apt preferences TESTING
+#tee -a /etc/apt/preferences  << END
+#Package: rsync
+#Pin: release n=bullseye
+#Pin-Priority: 900
+#END
 
 # update repositories
 apt update && apt full-upgrade -y
@@ -16,7 +16,7 @@ apt update && apt full-upgrade -y
 apt install fimware-linux firmware-sof-signed firmware-realtek -y
 
 # desktop environment
-apt install adwaita-icon-theme at-spi2-core baobab caribou dconf-cli dconf-gsettings-backend eog evince evolution-data-server fonts-cantarell gdm3 gkbd-capplet glib-networking gnome-backgrounds gnome-bluetooth gnome-calculator gnome-characters gnome-contacts gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-menus gnome-online-accounts gnome-online-miners gnome-session gnome-settings-daemon gnome-shell gnome-software gnome-sushi gnome-system-monitor gnome-terminal gnome-text-editor gnome-themes-extra gnome-user-docs gnome-user-share gsettings-desktop-schemas gstreamer1.0-packagekit gstreamer1.0-plugins-base gstreamer1.0-plugins-good gvfs-backends gvfs-fuse libatk-adaptor libcanberra-pulse libglib2.0-bin libpam-gnome-keyring libproxy1-plugin-gsettings libproxy1-plugin-webkit librsvg2-common nautilus pipewire-pulse sound-theme-freedesktop system-config-printer-common system-config-printer-udev tracker xdg-desktop-portal-gnome yelp zenity libproxy1-plugin-networkmanager network-manager-gnome file-roller gnome-tweaks gnome-weather gnome-calendar gnome-clocks geary transmission-gtk shotwell gnome-shell-extension-dash-to-panel totem cheese -y
+apt install adwaita-icon-theme at-spi2-core baobab caribou dconf-cli dconf-gsettings-backend eog evince evolution-data-server fonts-cantarell gdm3 gkbd-capplet glib-networking gnome-backgrounds gnome-bluetooth gnome-calculator gnome-characters gnome-contacts gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-menus gnome-online-accounts gnome-online-miners gnome-session gnome-settings-daemon gnome-shell gnome-software gnome-sushi gnome-system-monitor gnome-terminal gnome-text-editor gnome-themes-extra gnome-user-docs gnome-user-share gsettings-desktop-schemas gstreamer1.0-packagekit gstreamer1.0-plugins-base gstreamer1.0-plugins-good gvfs-backends gvfs-fuse libatk-adaptor libcanberra-pulse libglib2.0-bin libpam-gnome-keyring libproxy1-plugin-gsettings libproxy1-plugin-webkit librsvg2-common nautilus pulseaudio sound-theme-freedesktop system-config-printer-common system-config-printer-udev tracker xdg-desktop-portal-gnome yelp zenity libproxy1-plugin-networkmanager network-manager-gnome file-roller gnome-tweaks gnome-weather gnome-calendar gnome-clocks geary transmission-gtk shotwell gnome-shell-extension-dash-to-panel totem cheese -y
 
 # apps & utilities
 apt install gimp vim htop neofetch timeshift vlc papirus-icon-theme net-tools curl build-essential apt-transport-https python3-pip mlocate ffmpeg unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-ugly apt-file -y
@@ -57,7 +57,7 @@ apt update && apt install code -y
 # flatpak
 apt install flatpak gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.onlyoffice.desktopeditors com.system76.Popsicle io.gitlab.librewolf-community -y
+flatpak install flathub org.gtk.Gtk3theme.Adwaita-dark org.onlyoffice.desktopeditors com.system76.Popsicle io.gitlab.librewolf-community -y
 
 # locale
 sed -i 's/# it_IT.UTF-8 UTF-8/it_IT.UTF-8 UTF-8/g' /etc/locale.gen
@@ -80,6 +80,10 @@ END
 apt purge bluez avahi-autoipd -y
 apt autoremove -y
 
+# disable pipewire
+systemctl disable --global pipewire
+rm -rf /home/fabri/.config/pulse
+
 # swappiness
 tee -a /etc/sysctl.conf  << END
 vm.swappiness=10
@@ -90,6 +94,9 @@ tee -a /etc/modprobe.d/iwlwifi.conf  << END
 options iwlwifi enable_ini=N
 END
 
+# various
+tlp start
+
 # mdevctl fix
-mkdir /etc/mdevctl.d
+#mkdir /etc/mdevctl.d
 
