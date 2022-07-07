5b8c8ccd2aa9cb563afcc048cc8b3cacb58b2165
fabry76 <fabrizio.fabiani@gmail.com>
2022-07-07 17:24:22 +0200

    new

--- arch.sh
+++ arch.sh
@@ -38,15 +38,6 @@ systemctl enable cups
 sed -i 's/resolve/mdns_minimal [NOTFOUND=return] resolve/g' /etc/nsswitch.conf
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
 
-#flatpak
-#pacman -S flatpak --noconfirm
-#flatpak install flathub org.gtk.Gtk3theme.Breeze-Dark org.onlyoffice.desktopeditors -y
-#flatpak override --user --env=GTK_THEME=Breeze-Dark
-
-#disable pipewire
-#systemctl disable --global pipewire
-#rm -rf /home/fabri/.config/pulse
-
 #grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
