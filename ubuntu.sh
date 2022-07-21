fb7ba2527186afd2be636ac799942b5c1a82415a
fabry76 <fabrizio.fabiani@gmail.com>
2022-07-21 18:49:12 +0200

    new

--- ubuntu.sh
+++ ubuntu.sh
@@ -29,6 +29,13 @@ apt install sane printer-driver-all printer-driver-cups-pdf simple-scan -y
 usermod -a -G lpadmin fabri
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
 
+# fastgate
+apt install cifs-utils smbclient -y
+tee -a /etc/fstab  << END
+# map fastgate usb storage
+//192.168.1.254/samba/usb1_1 /home/fabri/Fastgate cifs user=admin,vers=1.0,dir_mode=0777,file_mode=0777,pass=admin
+END
+
 # grub
 sed -i 's/quiet/quiet loglevel=3/g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
