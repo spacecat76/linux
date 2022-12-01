ad92f91a4d00edd88cc8d7f831e4384a4f04f872
fabry76 <fabrizio.fabiani@gmail.com>
2022-12-01 13:20:47 +0100

    new

--- i3_user.sh
+++ i3_user.sh
@@ -7,7 +7,7 @@ cp ~/Git/linux/.config/i3blocks/config ~/.config/i3blocks/config
 cp ~/Git/linux/.config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
 cp ~/Git/linux/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
 cp ~/Git/linux/.config/systemd/user/redshift.service ~/.config/systemd/user/redshift.service
-cp ~/Git/linux/.config/re.conf ~/.config/redshift.conf
+cp ~/Git/linux/.config/redshift.conf ~/.config/redshift.conf
 
 # start redshift user service
 systemctl --user enable redshift
