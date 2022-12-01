32952092a3e1cbac0da8b59076098fefd5e9f780
fabry76 <fabrizio.fabiani@gmail.com>
2022-12-01 13:05:20 +0100

    new

--- i3_user.sh
+++ i3_user.sh
@@ -1,7 +1,13 @@
+# create directories
 mkdir -p ~/.config/{i3,i3blocks,xfce4/{terminal},gtk-3-0,systemd/{user}}
+
+# copy files
 cp ~/Git/linux/.config/i3/config ~/.config/i3/config
 cp ~/Git/linux/.config/i3block/config ~/.config/i3blocks/config
 cp ~/Git/linux/.config/xfce4/terminal/terminalrc ~/.config/xfce4/terminal/terminalrc
 cp ~/Git/linux/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
 cp ~/Git/linux/.config/systemd/user/redshift.service ~/.config/systemd/user/redshift.service
 cp ~/Git/linux/.config/re.conf ~/.config/redshift.conf
+
+# start redshift user service
+systemctl --user enable redshift
