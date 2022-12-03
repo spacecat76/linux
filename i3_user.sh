1e2665925e8656d1fb407c1c489fbcf414ef04e0
fabry76 <fabrizio.fabiani@gmail.com>
2022-12-03 18:27:53 +0100

    new

--- i3_user.sh
+++ i3_user.sh
@@ -1,9 +1,10 @@
 # create directories
-mkdir -p ~/.config/{i3,i3blocks,lxterminal,gtk-3.0,systemd/user}
+mkdir -p ~/.config/{i3,polybar,lxterminal,gtk-3.0,systemd/user}
 
 # copy files
 cp ~/Git/linux/.config/i3/config ~/.config/i3/config
-cp ~/Git/linux/.config/i3blocks/config ~/.config/i3blocks/config
+cp ~/Git/linux/.config/polybar/config ~/.config/polybar/config
+cp ~/Git/linux/.config/polybar/launch.sh ~/.config/polybar/launch.sh
 cp ~/Git/linux/.config/lxterminal/lxterminal.conf ~/.config/lxterminal/lxterminal.conf
 cp ~/Git/linux/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
 cp ~/Git/linux/.config/systemd/user/redshift.service ~/.config/systemd/user/redshift.service
@@ -15,3 +16,6 @@ systemctl --user enable redshift
 # gnome network manager
 gsettings set org.gnome.nm-applet disable-connected-notifications "true"
 gsettings set org.gnome.nm-applet disable-disconnected-notifications "true"
+
+# polybar
+chmod +x ~/.config/polybar/launch.sh
