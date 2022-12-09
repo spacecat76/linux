41c20ffe39e310fa7a6a7281b9d2fc48dc2b08e0
fabry76 <fabrizio.fabiani@gmail.com>
2022-12-09 16:08:15 +0100

    new

--- i3_user.sh
+++ i3_user.sh
@@ -1,5 +1,5 @@
 # create directories
-mkdir -p ~/.config/{i3,polybar/scripts,lxterminal,gtk-3.0,systemd/user}
+mkdir -p ~/.config/{i3,polybar/scripts,lxterminal,gtk-3.0,systemd/user,Thunar}
 
 # copy files
 cp ~/Git/linux/.config/i3/config ~/.config/i3/config
@@ -8,6 +8,7 @@ cp ~/Git/linux/.config/polybar/launch.sh ~/.config/polybar/launch.sh
 cp ~/Git/linux/.config/polybar/scripts/updates.sh ~/.config/polybar/scripts/updates.sh
 cp ~/Git/linux/.config/lxterminal/lxterminal.conf ~/.config/lxterminal/lxterminal.conf
 cp ~/Git/linux/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+cp ~/Git/linux/.config/Thunar/uca.xml ~/.config/Thunar/uca.xml
 cp ~/Git/linux/.config/systemd/user/redshift.service ~/.config/systemd/user/redshift.service
 cp ~/Git/linux/.config/redshift.conf ~/.config/redshift.conf
 
