1dc9c2c1e16f783eef40a821f7e0e4004df75136
fabry76 <fabrizio.fabiani@gmail.com>
2023-01-08 13:53:33 +0100

    new

--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 n1=$(apt list --upgradable | wc -l)
-n2=1
-echo $[n1 - n2]
+n2=$[ n1 - 1 ]
+if [ $n2 -ne 0 ];
+then
+    notify-send "UPDATES" "There are $n2 updates available"
+fi
+echo $n2
