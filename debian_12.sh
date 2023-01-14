e36929fd43a822c2e1eb0a5b9abd001cca870d52
fabry76 <fabrizio.fabiani@gmail.com>
2023-01-14 15:49:17 +0100

    new

--- debian_12.sh
+++ debian_12.sh
@@ -5,13 +5,13 @@ apt update && apt upgrade -y
 apt install firmware-linux firmware-sof-signed firmware-realtek -y
 
 # desktop environment
-apt install gnome-core gnome-weather gnome-calendar gnome-clocks gnome-screenshot gnome-tweaks file-roller transmission-gtk shotwell cheese -y
+apt install gnome-core gnome-weather gnome-calendar gnome-clocks gnome-tweaks file-roller transmission-gtk shotwell cheese -y
 
 # apps & utilities
-apt install gimp vim htop neofetch timeshift unrar net-tools curl build-essential apt-transport-https apt-file -y
+apt install gimp vim htop neofetch timeshift unrar net-tools curl build-essential apt-transport-https apt-file lm-sensors plymouth-themes -y
 
 # multimedia
-apt install vlc ffmpeg libavcodec-extra gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-ugly -y
+apt install vlc ffmpeg ffmpegfs libavcodec-extra gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad -y
 
 # fonts & icons
 apt install ttf-mscorefonts-installer fonts-ubuntu fonts-crosextra-carlito fonts-crosextra-caladea fonts-firacode papirus-icon-theme -y
@@ -22,7 +22,6 @@ systemctl enable avahi-daemon
 systemctl enable ufw --now
 ufw enable
 ufw allow mdns
-sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
 
 # virt manager
 apt install virt-manager -y
@@ -50,16 +49,19 @@ apt update && apt install code -y
 # flatpak
 apt install flatpak gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.gtk.Gtk3theme.Adwaita-dark com.system76.Popsicle org.libreoffice.LibreOffice -y
+flatpak install flathub org.gtk.Gtk3theme.Adwaita-dark org.libreoffice.LibreOffice -y
 
 # locale
 sed -i 's/# it_IT.UTF-8 UTF-8/it_IT.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 
 # grub
-sed -i 's/quiet/quiet loglevel=3/g' /etc/default/grub
-sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+sed -i 's/quiet/quiet loglevel=3 splash/g' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=2/g' /etc/default/grub
 update-grub
+
+# plymouth themes
+plymouth-set-default-theme -R lines
 
 # fastgate
 apt install cifs-utils smbclient -y
