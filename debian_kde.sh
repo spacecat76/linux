15317d65ea121c9f6b3e3c6ec92d136f6f3191a8
fabry76 <fabrizio.fabiani@gmail.com>
2023-03-17 20:35:01 +0100

    new

---
+++ debian_kde.sh
@@ -0,0 +1,64 @@
+# update repositories
+apt update && apt upgrade -y
+
+# firmware
+apt install firmware-linux firmware-sof-signed firmware-realtek -y
+
+# desktop environment
+apt install kde-plasma-desktop kcalc kde-spectacle okular libreoffice-kde5 -y
+
+# apps & utilities
+apt install gimp vim htop neofetch timeshift unrar net-tools curl build-essential apt-transport-https apt-file lm-sensors plymouth-themes cockpit cockpit-machines virt-viewer snapd libreoffice libreoffice-style-breeze -y
+
+# multimedia
+apt install vlc ffmpeg ffmpegfs libavcodec-extra gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad -y
+
+# fonts & icons
+apt install ttf-mscorefonts-installer fonts-ubuntu fonts-crosextra-carlito fonts-crosextra-caladea fonts-firacode papirus-icon-theme -y
+
+# snaps
+snap install core
+snap install code --classic
+
+# network
+apt install avahi-daemon ufw -y
+systemctl enable avahi-daemon
+systemctl enable ufw --now
+ufw enable
+ufw allow mdns
+sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
+
+# printing and scanning
+apt install sane cups printer-driver-all printer-driver-cups-pdf simple-scan -y
+systemctl enable cups
+usermod -a -G lpadmin fabri
+echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
+
+# google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /home/fabri
+apt install /home/fabri/google-chrome-stable_current_amd64.deb -y
+rm -f /home/fabri/google-chrome-stable_current_amd64.deb
+
+# locale
+sed -i 's/# it_IT.UTF-8 UTF-8/it_IT.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+
+# grub
+sed -i 's/quiet/quiet loglevel=3 splash/g' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=2/g' /etc/default/grub
+update-grub
+
+# plymouth themes
+plymouth-set-default-theme -R lines
+
+# fastgate
+apt install cifs-utils smbclient -y
+tee -a /etc/fstab  << END
+# map fastgate usb storage
+//192.168.1.254/samba/usb1_1 /home/fabri/Fastgate cifs user=admin,vers=1.0,dir_mode=0777,file_mode=0777,pass=admin
+END
+
+# gnome extensions
+apt purge gnome-shell-extensions -y
+apt install gnome-shell-extension-dashtodock -y
+apt autoremove -y
