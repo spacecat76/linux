bfd4d2cdecbc74d62d4975acdeb8c6c474337b36
fabry76 <fabrizio.fabiani@gmail.com>
2023-05-14 17:09:09 +0200

    new

--- arch.sh
+++ arch.sh
@@ -39,5 +39,5 @@ sed -i 's/resolve/mdns_minimal [NOTFOUND=return] resolve/g' /etc/nsswitch.conf
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
 
 #grub
-sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=2/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
