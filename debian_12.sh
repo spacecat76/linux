f2e9e3230bde7c448a21be22753218fc6d95d857
fabry76 <fabrizio.fabiani@gmail.com>
2023-05-14 20:39:26 +0200

    new

--- debian_12.sh
+++ debian_12.sh
@@ -8,7 +8,7 @@ apt install firmware-linux firmware-sof-signed firmware-realtek chromium -y
 apt install gnome-core gnome-boxes gnome-weather gnome-calendar gnome-clocks gnome-tweaks file-roller transmission-gtk shotwell cheese seahorse -y
 
 # apps & utilities
-apt install gimp vim htop neofetch timeshift unrar net-tools curl build-essential apt-transport-https apt-file lm-sensors plymouth-themes gnome-boxes -y
+apt install gimp vim htop neofetch timeshift unrar net-tools curl build-essential apt-transport-https apt-file lm-sensors plymouth-themes -y
 
 # multimedia
 apt install vlc ffmpeg ffmpegfs libavcodec-extra gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad -y
