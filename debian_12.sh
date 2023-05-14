76e23ba9021828f56e3512bafa68469a30397e86
fabry76 <fabrizio.fabiani@gmail.com>
2023-05-14 18:47:47 +0200

    new

--- debian_12.sh
+++ debian_12.sh
@@ -5,10 +5,10 @@ apt update && apt upgrade -y
 apt install firmware-linux firmware-sof-signed firmware-realtek -y
 
 # desktop environment
-apt install gnome-core gnome-weather gnome-calendar gnome-clocks gnome-tweaks file-roller transmission-gtk shotwell cheese seahorse libreoffice-gnome -y
+apt install gnome-core gnome-weather gnome-calendar gnome-clocks gnome-tweaks file-roller transmission-gtk shotwell cheese seahorse libreoffice -y
 
 # apps & utilities
-apt install gimp vim htop neofetch timeshift unrar net-tools curl build-essential apt-transport-https apt-file lm-sensors plymouth-themes gnome-boxes libreoffice libreoffice-style-breeze -y
+apt install gimp vim htop neofetch timeshift unrar net-tools curl build-essential apt-transport-https apt-file lm-sensors plymouth-themes gnome-boxes -y
 
 # multimedia
 apt install vlc ffmpeg ffmpegfs libavcodec-extra gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad -y
@@ -53,6 +53,11 @@ update-grub
 
 # plymouth themes
 plymouth-set-default-theme -R lines
+
+# flatpak
+apt install flatpak gnome-software-plugin-flatpak -y
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.gtk.Gtk3theme.Adwaita-dark org.mozilla.firefox io.gitlab.librewolf-community org.libreoffice.LibreOffice -y
 
 # fastgate
 apt install cifs-utils smbclient -y
