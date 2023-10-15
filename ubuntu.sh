113fe460b89f72fd0f3f71e7f2e9b0403f96a6ac
Fabrizio <fabrizio.fabiani@gmail.com>
2023-10-15 20:55:19 +0200

    new

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,12 +1,12 @@
 # install applications
-apt install ubuntu-restricted-extras gnome-shell-extension-manager gnome-weather gnome-calendar gnome-clocks gnome-tweaks vlc ffmpegthumbnailer timeshift neofetch curl wget htop net-tools apt-transport-https vim cheese shotwell -y
+apt install ubuntu-restricted-extras gnome-shell-extension-manager gnome-weather gnome-calendar gnome-clocks gnome-tweaks vlc ffmpegthumbnailer timeshift neofetch curl wget htop net-tools apt-transport-https vim cheese shotwell transmission -y
 
 # fonts & icons
 apt install fonts-crosextra-carlito fonts-crosextra-caladea -y
 
 # snaps
 snap install code --classic
-snap install gnome-boxes gimp transmission onlyoffice-desktopeditors
+snap install gnome-boxes gimp onlyoffice-desktopeditors
 
 # chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /home/fabri/Downloads
