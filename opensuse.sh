8bc852f5bfa0f01ed453392fd76c0e460d028b85
fabry76 <fabrizio.fabiani@gmail.com>
2023-12-10 17:50:23 +0100

    new

--- opensuse.sh
+++ opensuse.sh
@@ -10,4 +10,3 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo zypper addrepo https://packages.microsoft.com/yumrepos/vscode vscode
 sudo zypper refresh
 sudo zypper install code
-
