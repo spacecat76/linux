c088a87a3cceb6bb771956c631d57d32c744d5f3
fabry76 <fabrizio.fabiani@gmail.com>
2023-12-11 20:29:02 +0100

    new

--- opensuse.sh
+++ opensuse.sh
@@ -1,12 +1,12 @@
 # chrome
 wget https://dl.google.com/linux/linux_signing_key.pub
-sudo rpm --import linux_signing_key.pub
-sudo zypper ar http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
-sudo zypper ref
-sudo zypper in google-chrome-stable
+rpm --import linux_signing_key.pub
+zypper ar http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
+zypper ref
+zypper in google-chrome-stable
 
 # vscode
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo zypper addrepo https://packages.microsoft.com/yumrepos/vscode vscode
-sudo zypper refresh
-sudo zypper install code
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+zypper addrepo https://packages.microsoft.com/yumrepos/vscode vscode
+zypper refresh
+zypper install code
