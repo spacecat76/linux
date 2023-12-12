152ae4efb5cf518220d39515dacbc79d934a02d0
fabry76 <fabrizio.fabiani@gmail.com>
2023-12-12 17:40:37 +0100

    new

--- opensuse.sh
+++ opensuse.sh
@@ -2,7 +2,7 @@
 wget https://dl.google.com/linux/linux_signing_key.pub
 rpm --import linux_signing_key.pub
 zypper ar http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
-zypper ref
+zypper refresh
 zypper in google-chrome-stable
 
 # vscode
