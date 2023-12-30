b8ac1372b1f4128dcc0842710dc2e98130f761cb
fabry76 <fabrizio.fabiani@gmail.com>
2023-12-30 20:15:48 +0100

    new

--- debian_11.sh
+++ debian_11.sh
@@ -49,6 +49,7 @@ rm -f /home/fabri/google-chrome-stable_current_amd64.deb
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+# sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
 rm -f packages.microsoft.gpg
 apt update && apt install code -y
 
