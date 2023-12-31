0d6930353305c8cc5eb684e42a2039eb771d4502
fabry76 <fabrizio.fabiani@gmail.com>
2023-12-31 18:11:56 +0100

    new

--- popos.sh
+++ popos.sh
@@ -1,5 +1,5 @@
 # applications
-apt install neofetch htop shotwell apt-transport-https code transmission cheese gimp vim gnome-tweaks libreoffice-style-breeze google-chrome-stable gnome-screenshot -y
+apt install neofetch htop shotwell apt-transport-https code transmission cheese gimp vim gnome-tweaks libreoffice-style-breeze google-chrome-stable -y
 
 # multimedia
 apt install vlc ubuntu-restricted-extras -y
@@ -10,10 +10,6 @@ sed -i 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
 systemctl enable ufw --now
 ufw enable
 ufw allow mdns
-
-# virt manager
-apt install virt-manager -y
-virsh net-autostart default
 
 # printing and scanning
 apt install printer-driver-cups-pdf -y
@@ -26,6 +22,3 @@ tee -a /etc/fstab  << END
 //192.168.1.254/samba/usb1_1 /home/fabri/Fastgate cifs user=admin,vers=1.0,dir_mode=0777,file_mode=0777,pass=admin
 END
 
-# varie
-systemctl disable bluetooth
-apt autoremove -y
