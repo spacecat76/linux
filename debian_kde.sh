89de72280210646f35e2ad83c8fd2348caa3bfb4
fabry76 <fabrizio.fabiani@gmail.com>
2024-01-03 18:00:55 +0100

    new

--- debian_kde.sh
+++ debian_kde.sh
@@ -20,7 +20,7 @@ apt install shotwell tlp pkexec timeshift vim htop neofetch unrar net-tools curl
 apt install vlc ffmpeg ffmpegfs libavcodec-extra gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly -y
 
 # fonts & icons
-apt install ttf-mscorefonts-installer fonts-ubuntu fonts-crosextra-carlito fonts-crosextra-caladea fonts-firacode papirus-icon-theme -y
+apt install ttf-mscorefonts-installer fonts-ubuntu fonts-crosextra-carlito fonts-crosextra-caladea papirus-icon-theme -y
 
 # snaps
 apt install snapd -y
