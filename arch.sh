d8ba39bf9204a386dae4c6e5f69c30917c1945c5
Fabrizio <fabri@debian.lan>
2024-03-06 20:26:30 +0100

    new

--- arch.sh
+++ arch.sh
@@ -1,5 +1,5 @@
 #kde
-pacman -S --needed plasma plasma-wayland-session plasma-wayland-protocols sddm ark dolphin konsole okular galculator kate spectacle packagekit-qt5 print-manager system-config-printer ksystemlog partitionmanager kamoso gwenview --noconfirm
+pacman -S --needed plasma plasma-wayland-session plasma-wayland-protocols sddm ark dolphin konsole okular galculator kate spectacle packagekit-qt5 packagekit-qt6 kdialog print-manager system-config-printer ksystemlog partitionmanager kamoso gwenview --noconfirm
 
 #remove components
 pacman -Rd --nodeps plasma-browser-integration --noconfirm
@@ -8,27 +8,38 @@ pacman -Rd --nodeps plasma-browser-integration --noconfirm
 systemctl enable sddm
 
 #utilities
-pacman -S --needed papirus-icon-theme htop curl neofetch rust wget linux-lts-headers bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse ffmpeg pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack --noconfirm
+pacman -S --needed papirus-icon-theme curl neofetch rust wget linux-lts-headers bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse ffmpeg pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse --noconfirm
 
 #fonts
 pacman -S --needed ttf-ubuntu-font-family ttf-opensans ttf-carlito ttf-caladea ttf-liberation ttf-inconsolata ttf-fira-code ttf-fira-mono ttf-fira-sans --noconfirm
 
 #applications
-pacman -S --needed firefox vim nano vlc gimp transmission-qt pinta neofetch libreoffice-still --noconfirm
+pacman -S --needed firefox vim nano vlc gimp transmission-qt htop neofetch --noconfirm
 
 #network
 pacman -S --needed network-manager-applet nss-mdns inetutils net-tools avahi --noconfirm
 systemctl enable avahi-daemon
 sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
 
+# virt manager
+pacman -S virt-manager --noconfirm
+systemctl enable libvirtd.socket
+adduser fabri libvirt
+virsh net-autostart default
+sed -i 's/#user = "libvirt-qemu"/user = "fabri"/g' /etc/libvirt/qemu.conf
+sed -i 's/#group = "libvirt-qemu"/group = "libvirt"/g' /etc/libvirt/qemu.conf
+
 # ditrobox
 pacman -S --needed podman distrobox --noconfirm
 
+# flatpak
+pacman -S flatpak
+flatpak install flathub org.onlyoffice.desktopeditors -y
+
 #firewall
-pacman -S firewalld --noconfirm
-cp /home/fabri/Git/linux/etc/ffw.xml /usr/lib/firewalld/zones
-firewall-cmd --reload
-firewall-cmd --set-default-zone ffw
+pacman -S ufw --noconfirm
+ufw enable
+ufw allow mdns
 
 #printing and scanning
 pacman -S --needed sane cups gutenprint simple-scan --noconfirm
