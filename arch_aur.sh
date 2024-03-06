ef869a9c9df3cac3a2dd9ce879999523cb880967
Fabrizio <fabri@debian.lan>
2024-03-06 22:15:45 +0100

    new

--- arch_aur.sh
+++
@@ -1,7 +0,0 @@
-#paru
-git clone https://aur.archlinux.org/paru.git /home/fabri/Downloads/paru
-cd /home/fabri/Downloads/paru
-makepkg -sri
-
-#kde aur apps
-paru -S ttf-ms-fonts brave-bin --noconfirm
