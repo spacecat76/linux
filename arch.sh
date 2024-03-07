c22789d7a5e9df407b8766b4f54d4f05ea660f3d
Fabrizio <fabri@debian.lan>
2024-03-07 21:17:11 +0100

    new

--- arch.sh
+++ arch.sh
@@ -1,22 +1,22 @@
-#kde
+# kde
 pacman -S --needed plasma plasma-wayland-session plasma-wayland-protocols sddm ark dolphin konsole okular kalk kate spectacle packagekit-qt5 packagekit-qt6 kdialog print-manager system-config-printer ksystemlog partitionmanager kamoso gwenview --noconfirm
 
-#remove components
+# remove components
 pacman -Rd --nodeps plasma-browser-integration --noconfirm
 
-#sddm
+# sddm
 systemctl enable sddm
 
-#utilities
+# utilities
 pacman -S --needed papirus-icon-theme curl neofetch rust wget linux-headers bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse2 ffmpeg pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse --noconfirm
 
-#fonts
+# fonts
 pacman -S --needed ttf-ubuntu-font-family ttf-opensans ttf-carlito ttf-caladea ttf-liberation ttf-inconsolata ttf-fira-code ttf-fira-mono ttf-fira-sans --noconfirm
 
-#applications
+# applications
 pacman -S --needed firefox vim nano vlc gimp pinta transmission-qt htop neofetch --noconfirm
 
-#network
+# network
 pacman -S --needed network-manager-applet nss-mdns inetutils net-tools avahi --noconfirm
 systemctl enable avahi-daemon
 sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
@@ -37,18 +37,18 @@ pacman -S --needed flatpak --noconfirm
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.onlyoffice.desktopeditors io.gitlab.librewolf-community -y
 
-#firewall
+# firewall
 pacman -S --needed ufw --noconfirm
 ufw enable
 ufw allow mdns
 
-#printing and scanning
+# printing and scanning
 pacman -S --needed sane cups gutenprint simple-scan --noconfirm
 systemctl enable cups
 sed -i 's/resolve/mdns_minimal [NOTFOUND=return] resolve/g' /etc/nsswitch.conf
 echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
 
-#grub
+# grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=2/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
