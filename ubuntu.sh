5efc0d3cc3363a51e414e07a7d74390510bb4a3a
fabry76 <fabrizio.fabiani@gmail.com>
2024-03-15 18:01:02 +0000

    new

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,9 +9,9 @@ snap install gimp onlyoffice-desktopeditors
 snap install code --classic
 
 # chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -f ./google-chrome-stable_current_amd64.deb -y
-rm -f google-chrome-stable_current_amd64.deb
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#apt install -f ./google-chrome-stable_current_amd64.deb -y
+#rm -f google-chrome-stable_current_amd64.deb
 
 # firewall
 ufw enable
