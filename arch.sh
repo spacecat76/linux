272e22e83fee2a72fcb7d9e22f92289c2bbc6835
fabry76 <fabrizio.fabiani@gmail.com>
2024-03-20 22:53:16 +0100

    new

--- arch.sh
+++ arch.sh
@@ -1,3 +1,7 @@
+# enrolling the TPM
+#systemd-cryptenroll /dev/sda2 --recovery-key
+#systemd-cryptenroll /dev/sda2 --wipe-slot=empty --tpm2-device=auto
+
 # kde
 pacman -S --needed plasma kwalletmanager plasma-wayland-protocols sddm ark dolphin konsole okular kalk kate spectacle packagekit-qt5 packagekit-qt6 libxvmc kdialog print-manager system-config-printer ksystemlog partitionmanager kamoso gwenview --noconfirm
 
