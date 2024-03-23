b8c31605c99624260cc040f330f22808f0a33523
fabry76 <fabrizio.fabiani@gmail.com>
2024-03-23 12:09:41 +0100

    new

--- arch.sh
+++ arch.sh
@@ -11,7 +11,7 @@ pacman -Rd --nodeps plasma-browser-integration --noconfirm
 systemctl enable sddm
 
 # utilities
-pacman -S --needed timeshif profile-sync-daemon speech-dispatcher papirus-icon-theme curl neofetch rust wget bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse2 ffmpeg ffmpegthumbs pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse --noconfirm
+pacman -S --needed timeshift profile-sync-daemon speech-dispatcher papirus-icon-theme curl neofetch rust wget bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse2 ffmpeg ffmpegthumbs pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse --noconfirm
 
 # fonts
 pacman -S --needed ttf-ubuntu-font-family ttf-opensans ttf-carlito ttf-caladea ttf-liberation ttf-inconsolata ttf-fira-code ttf-fira-mono ttf-fira-sans --noconfirm
