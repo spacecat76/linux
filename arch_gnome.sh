95c0fad6c19337ca4a443deef7c85627a5336de0
fabry76 <fabrizio.fabiani@gmail.com>
2024-03-25 14:58:14 +0100

    new

---
+++ arch_gnome.sh
@@ -0,0 +1,44 @@
+# refresh mirrors
+pacman -Syyu
+
+# kde
+pacman -S --needed gnome gnome-tweaks file-roller seahorse shotwell cheese --noconfirm
+
+# applications
+pacman -S --needed firefox chromium vim nano vlc gimp transmission htop neofetch --noconfirm
+
+# utilities
+pacman -S --needed timeshift speech-dispatcher curl neofetch rust wget bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse2 ffmpeg ffmpegthumbs pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse --noconfirm
+
+# fonts & icons
+pacman -S --needed papirus-icon-theme ttf-ubuntu-font-family ttf-opensans ttf-carlito ttf-caladea ttf-liberation ttf-inconsolata ttf-fira-code ttf-fira-mono ttf-fira-sans --noconfirm
+
+# network
+pacman -S --needed network-manager-applet nss-mdns inetutils net-tools avahi --noconfirm
+systemctl enable avahi-daemon
+
+# virt manager
+pacman -S --needed qemu virt-manager dnsmasq --noconfirm
+systemctl enable libvirtd.socket
+usermod -a -G libvirt fabri
+sed -i 's/#user = "libvirt-qemu"/user = "fabri"/g' /etc/libvirt/qemu.conf
+sed -i 's/#group = "libvirt-qemu"/group = "libvirt"/g' /etc/libvirt/qemu.conf
+
+# firewall
+pacman -S --needed ufw --noconfirm
+systemctl enable ufw
+ufw enable
+ufw allow mdns
+
+# printing and scanning
+pacman -S --needed sane cups gutenprint --noconfirm
+systemctl enable cups
+sed -i 's/resolve/mdns_minimal [NOTFOUND=return] resolve/g' /etc/nsswitch.conf
+echo "bjnp://192.168.1.94" | tee -a /etc/sane.d/pixma.conf
+
+# fastgate
+pacman -S --needed cifs-utils samba smbclient --noconfirm
+tee -a /etc/fstab  << END
+# map fastgate usb storage
+//192.168.1.254/samba/usb1_1 /home/fabri/Fastgate cifs user=admin,vers=1.0,dir_mode=0777,file_mode=0777,pass=admin
+END
