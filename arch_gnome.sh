d3cfa5e41e100458bdd7ff8ff8c82dabd1f0153b
fabry76 <fabrizio.fabiani@gmail.com>
2024-03-29 23:44:45 +0100

    new

--- arch_gnome.sh
+++ arch_gnome.sh
@@ -14,7 +14,7 @@ pacman -Rd --nodeps epiphany --noconfirm
 pacman -S --needed firefox vim nano vlc gimp htop neofetch timeshift podman distrobox starship --noconfirm
 
 # utilities
-pacman -S --needed fwupd fuse-overlayfs speech-dispatcher curl neofetch rust wget bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse2 ffmpeg ffmpegthumbs gst-libav --noconfirm
+pacman -S --needed fwupd fuse-overlayfs speech-dispatcher curl neofetch rust wget bash-completion sof-firmware appstream mlocate unrar unzip p7zip fuse2 ffmpeg ffmpegthumbs gst-libav gst-plugins-ugly --noconfirm
 
 # flatpak
 pacman -S --needed flatpak --noconfirm
