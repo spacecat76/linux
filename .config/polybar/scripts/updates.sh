3e9d854bb2d8b7120b023585c63c7b3aa951d1f7
fabry76 <fabrizio.fabiani@gmail.com>
2024-09-22 20:51:37 +0200

    new

--- .config/polybar/scripts/updates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-n1=$(apt list --upgradable | wc -l)
-n2=$[ n1 - 1 ]
-if [ $n2 -ne 0 ];
-then
-    notify-send -t 30000 -i /usr/share/icons/Papirus-Dark/128x128/apps/update-notifier.svg "UPDATES" "There are $n2 updates available"
-fi
-echo $n2
