611096ceb5dd69523c1415fc8b9fc52fa613f960
fabry76 <fabrizio.fabiani@gmail.com>
2024-09-22 22:01:28 +0200

    new

--- arch_gnome.sh
+++ arch_gnome.sh
@@ -11,7 +11,7 @@ systemctl enable gdm
 pacman -Rd --nodeps epiphany --noconfirm
 
 # applications
-pacman -S --needed firefox vim nano vlc htop fastfetch timeshift podman distrobox starship --noconfirm
+pacman -S --needed firefox vim nano vlc htop fastfetch timeshift podman distrobox starship transmission-gtk --noconfirm
 systemctl enable cronie
 usermod --add-subuids 100000-165535 --add-subgids 100000-165535 fabri
 
@@ -21,7 +21,7 @@ pacman -S --needed fwupd speech-dispatcher curl neofetch rust wget bash-completi
 # flatpak
 pacman -S --needed flatpak --noconfirm
 flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install --system flathub org.onlyoffice.desktopeditors com.brave.Browser org.gimp.GIMP com.system76.Popsicle com.github.tchx84.Flatseal com.transmissionbt.Transmission com.mattjakeman.ExtensionManager com.spotify.Client -y
+flatpak install --system flathub org.onlyoffice.desktopeditors com.brave.Browser org.gimp.GIMP com.system76.Popsicle com.github.tchx84.Flatseal com.mattjakeman.ExtensionManager com.spotify.Client -y
 
 # fonts & icons
 pacman -S --needed papirus-icon-theme ttf-ubuntu-font-family ttf-opensans ttf-carlito ttf-caladea ttf-liberation ttf-inconsolata ttf-dejavu noto-fonts adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts nerd-fonts --noconfirm
