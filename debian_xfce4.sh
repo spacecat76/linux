1c108a132655c96fb94886effd1a4e6e4ad3acd6
fabry76 <fabrizio.fabiani@gmail.com>
2024-12-20 19:11:54 +0100

    new

---
+++ debian_xfce4.sh
@@ -0,0 +1,66 @@
+# update repositories
+apt update && apt upgrade -y
+
+# locale
+sed -i 's/# it_IT.UTF-8 UTF-8/it_IT.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+
+# firmware
+apt install firmware-linux firmware-sof-signed firmware-realtek -y
+
+# firefox
+install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+apt update && apt install firefox -y
+
+# desktop environment
+apt install transmission-gtk shotwell cheese -y
+
+# apps & utilities
+apt install timeshift vim htop neofetch unrar net-tools curl apt-file plymouth-themes apt-transport-https -y
+
+# multimedia
+apt install vlc ffmpeg ffmpegfs libavcodec-extra gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly -y
+
+# fonts & icons
+apt install yaru-theme-gnome-shell yaru-theme-icon papirus-icon-theme ttf-mscorefonts-installer fonts-ubuntu fonts-crosextra-carlito fonts-crosextra-caladea -y
+
+# chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -f ./google-chrome-stable_current_amd64.deb -y
+rm -f google-chrome-stable_current_amd64.deb
+
+# snap
+apt install snapd -y
+snap install core onlyoffice-desktopeditors spotify
+snap install code --classic
+
+# firewall
+apt install ufw -y
+sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
+ufw enable
+ufw allow mdns
+
+# printing and scanning
+apt install sane cups printer-driver-all printer-driver-cups-pdf simple-scan -y
+adduser fabri lpadmin
+
+# grub
+sed -i 's/quiet/quiet loglevel=3 splash/g' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=2/g' /etc/default/grub
+update-grub
+
+# plymouth themes
+plymouth-set-default-theme -R lines
+
+# fastgate
+apt install cifs-utils smbclient -y
+tee -a /etc/fstab  << END
+# map fastgate usb storage
+//192.168.1.254/samba/usb1_1 /home/fabri/Fastgate cifs user=admin,vers=1.0,dir_mode=0777,file_mode=0777,pass=admin
+END
+
+# various
+sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=30s/g' /etc/systemd/system.conf
+sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=30s/g' /etc/systemd/user.conf
