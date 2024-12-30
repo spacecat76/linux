7a9f55316af2a6cafbe964110a6144ab99f47a22
Fabrizio <87930958+fabry76@users.noreply.github.com>
2024-12-30 23:55:01 +0100

    new

--- debian_13.sh
+++ debian_13.sh
@@ -8,12 +8,6 @@ locale-gen
 # firmware
 apt install firmware-linux firmware-sof-signed firmware-realtek -y
 
-# firefox
-install -d -m 0755 /etc/apt/keyrings
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-apt update && apt install firefox -y
-
 # desktop environment
 apt install gnome-core gnome-shell-extension-dash-to-panel gnome-shell-extension-dashtodock gnome-weather gnome-calendar gnome-clocks gnome-tweaks file-roller seahorse transmission-gtk shotwell gnome-snapshot -y
 
