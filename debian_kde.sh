95369a92bd65d4f1b7c8715a6ad30758495bb57d
fabry76 <fabrizio.fabiani@gmail.com>
2025-03-02 13:42:41 +0100

    new

--- debian_kde.sh
+++ debian_kde.sh
@@ -76,7 +76,7 @@ tee -a /etc/fstab  << END
 END
 
 # remove components
-apt purge plasma-browser-integration konqueror zutty avahi-autoipd -y
+#apt purge plasma-browser-integration konqueror zutty avahi-autoipd -y
 apt autoremove -y
 
 # lid setting
